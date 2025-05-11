29316ddc2a14ca0fb8a889fa24f5da5245433079
Agasalim <aga77ams@gmail.com>
2025-05-11 22:24:00 +0400

    6-th commit axtaris bolumu mobile design elave olundu

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 let menu = document.querySelector(".menu_bar");
 let logo = document.querySelector(".logo");
 let searchLogin = document.querySelector(".search_login");
-let elanBtn = document.querySelector(".elan_btn");
 let links = document.querySelector(".links");
 let contact = document.querySelector(".contact");
 let fav_lang = document.querySelector(".fav_lang");
@@ -14,12 +13,9 @@ menu.onclick = function(){
         contact.style.left = 0;
         logo.style.position = "absolute";
         searchLogin.style.display = "none";
-        elanBtn.style.display = "none";
-        $(elanBtn).fadeOut(300);
         menu.style.right = "5%"
         menu.style.left = "auto"
         menu.style.backgroundColor = "var(--color4)"
-        topLine.style.height = "100vh"
     }
     else{
         fav_lang.style.left = "-100%"
@@ -27,10 +23,8 @@ menu.onclick = function(){
         contact.style.left = "-100%";
         logo.style.position = "static";
         searchLogin.style.display = "flex";
-        $(elanBtn).fadeIn(300);
         menu.style.right = "auto"
         menu.style.left = "5%"
         menu.style.backgroundColor = "transparent"
-        topLine.style.height = "fit-content"
     }
 }
