a4953fa7bf2ebb58972902851ea76d59bf320654
Agasalim <aga77ams@gmail.com>
2025-05-11 14:11:19 +0400

    4-th commit

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ let menu = document.querySelector(".menu_bar");
 
 menu.onclick = function(){
     if($("#checkbox").is(":checked")){
-        $(".contact").slideDown();
+        document.querySelector(".fav_lang").style.display = "flex"
         $(".links").slideDown();
-        $(".third_line").slideDown();
+        $(".contact").slideDown();
+        document.querySelector(".third_line").style.display = "flex";
+        document.querySelector(".first_line").style.display = "none";
+        document.querySelector(".menu_bar").style.right = "0"
+        document.querySelector(".menu_bar").style.left = "auto"
+        document.querySelector(".menu_bar").style.backgroundColor = "var(--color4)"
     }
     else{
-        $(".contact").slideUp();
+        document.querySelector(".fav_lang").style.display = "none"
         $(".links").slideUp();
-        $(".third_line").slideUp();
+        $(".contact").slideUp();
+        document.querySelector(".third_line").style.display = "none";
+        document.querySelector(".first_line").style.display = "flex";
+        document.querySelector(".menu_bar").style.right = "auto"
+        document.querySelector(".menu_bar").style.left = "0"
+        document.querySelector(".menu_bar").style.backgroundColor = "transparent"
     }
 }
