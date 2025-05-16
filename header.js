fee2bdc6b5c01de3aca4b65d95a8df0fc0a7b05b
Agasalim <aga77ams@gmail.com>
2025-05-16 18:06:31 +0400

    10-th commit

--- header.js
+++ header.js
@@ -3,8 +3,10 @@ let sifirla = document.querySelector(".sifirla");
 let etrafliConetnt = document.querySelector(".etrafli_axtar_conetnt");
 
 function etrafliAxtar(){
+    // $(etrafliConetnt).slideDown()
+    // etrafliConetnt.style.maxHeight = etrafliConetnt.scrollHeight + "px";
+    // etrafliConetnt.style.opacity = "1";
+    etrafliConetnt.style.display = "flex"
     etrafli.style.display = "none";
     sifirla.style.display = "initial";
-    etrafliConetnt.style.display = "flex";
-    $(etrafliConetnt).slideDown()
 }
